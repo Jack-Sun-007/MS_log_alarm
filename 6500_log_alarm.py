4a0b27d401c6f214c70dbd69e1be8e322d4505e1
Jack-Sun-007 <62201888+Jack-Sun-007@users.noreply.github.com>
2020-11-10 09:44:12 +0800

    Add files via upload

---
+++ 6500_log_alarm.py
@@ -0,0 +1,69 @@
+﻿# -*- coding: utf-8 -*-
+import mmap
+import contextlib
+import re
+import requests
+import json
+import datetime
+import time
+
+from Evtx.Evtx import FileHeader
+from Evtx.Views import evtx_file_xml_view
+from xml.dom import minidom
+
+#获取前1小时的日期和时间（日志时间是+0时区）
+time1= (datetime.datetime.now()+datetime.timedelta(hours=-9)).strftime("%Y-%m-%d %H:")
+
+#定义发送钉钉报警的函数
+def send_msg(text,mobile):
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    json_text = {
+                "msgtype": "text",
+                "text": {
+                    "content": text
+                },
+                "at": {
+                    "atMobiles": [
+                        mobile
+                    ],
+                    "isAtAll": False
+                }
+            }
+    requests.post(url, json.dumps(json_text), headers=headers).content
+
+#钉钉联系人
+abc = "\"130xxxxxxxx\""
+
+error_info="6500质谱ERROR信息：\n"
+with open('C:\\Windows\\System32\\winevt\\Logs\\Application.evtx', 'r') as f:
+    with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as buf:
+        fh = FileHeader(buf, 0)
+        for xml, record in evtx_file_xml_view(fh):
+            # 得到文档对象
+            dom = minidom.parseString(xml)
+            # 得到元素对象
+            element = dom.documentElement
+            # 获得子标签
+            timeElement = element.getElementsByTagName("TimeCreated")
+            # 获得子标签属性值
+            Time = timeElement[0].getAttribute("SystemTime")
+            if time1 in str(Time):
+                nameElement = element.getElementsByTagName("Provider")
+                name = nameElement[0].getAttribute("Name")
+                if name == "Analyst":
+                    error = element.getElementsByTagName("Level")[0].firstChild.data
+                    if error == "2":
+                        EventID = element.getElementsByTagName("EventID")[0].firstChild.data
+                        if EventID == "43":
+                            error_info = (error_info + "\n仪器报错但无信息\n")
+                        else:
+                            dataElement = element.getElementsByTagName("Data")
+                            dataInfo = dataElement[0].firstChild.data
+                            info = re.sub(r'<[^>]+>', '', dataInfo)
+                            error_info = (error_info + info)
+f.close()
+print(len(error_info))
+if len(error_info) > 23:
+    print(error_info)
+    send_msg(error_info, abc)
---
+++ HFX_log_alarm.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import codecs
+import re
+import datetime
+import requests
+import json
+import time
+
+#获取前1小时的日期和时间
+time1= (datetime.datetime.now()+datetime.timedelta(seconds=-3600)).strftime("%Y-%m-%d %H:")
+
+#定义发送钉钉报警的函数
+def send_msg(text,mobile):
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    json_text = {
+                "msgtype": "text",
+                "text": {
+                    "content": text
+                },
+                "at": {
+                    "atMobiles": [
+                        mobile
+                    ],
+                    "isAtAll": False
+                }
+            }
+    requests.post(url, json.dumps(json_text), headers=headers).content
+
+#钉钉联系人
+abc = "\"130xxxxxxxx\""
+
+#打开log文件，获取每行日志信息至lines，并关闭log文件
+with open('C:\\Xcalibur\\system\\Exactive\\log\\Thermo Exactive--2020-09-01--12-48-15.log', 'r', encoding='utf_8_sig') as fp:
+    lines = fp.readlines()
+fp.close()
+
+#读取1小时内log文件中的ERROR和WARNING信息
+error_text = "HFX\n[**ERROR**]\n"
+warning_text = "HFX\n[***WARNING***]\n"
+for line in lines:
+    if time1 in line:
+        if "error" in line:
+            text = re.sub(u"\\[.*?]", "", line)
+            error_text = error_text + text
+        if "warning" in line:
+            text = re.sub(u"\\[.*?]", "", line)
+            warning_text = warning_text + text
+
+#如果日志出现报警就发送钉钉提醒
+for line in lines:
+    if time1 in line:
+        if "error" in line:
+            print(error_text)
+            send_msg(error_text, abc)
+        if "warning" in line:
+            print(warning_text)
+            send_msg(warning_text, abc)
