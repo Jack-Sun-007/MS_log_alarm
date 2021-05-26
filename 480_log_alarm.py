dd8027825607656a1492c382bfbb9b14008495be
Jack-Sun-007 <62201888+Jack-Sun-007@users.noreply.github.com>
2021-05-26 13:44:26 +0800

    Update 480_log_alarm.py
    重构代码，修复日志内容显示重复和不全的问题，新增以分钟进行日志筛选

--- 480_log_alarm.py
+++ 480_log_alarm.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
-import codecs
+import json
+import time
 import datetime
 import requests
-import json
 
-#定义发送钉钉报警的函数
-def send_msg(text,mobile):
+
+# 定义发送钉钉报警的函数
+def send_msg(text, mobile):
     url = 'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     headers = {'Content-Type': 'application/json;charset=utf-8'}
     json_text = {
@@ -23,42 +24,70 @@ def send_msg(text,mobile):
             }
     requests.post(url, json.dumps(json_text), headers=headers).content
 
-#钉钉联系人
-abc = "\"130xxxxxxxx\""
 
-#获取当天日期和时间
-time1 = datetime.datetime.now().strftime("%Y-%m-%d %H:")
-date1 = datetime.datetime.now().strftime("%Y-%m-%d")
+# 钉钉联系人
+def person():
+    who = "\"132xxxxxxxx\""
+    return who
 
-#读取当天log日志文件路径
-p = 'C:\\Thermo\\Instruments\\Exploris\\2.0\\System\\Programs\\dependencies\\logs\\'
-for filename in os.listdir(p):
-        value = os.path.join(p,filename)
+
+# 读取当天log日志文件路径
+def get_log_path():
+    date1 = datetime.datetime.now().strftime('%Y-%m-%d')
+    p = 'C:\\Thermo\\Instruments\\Exploris\\2.0\\System\\Programs\\dependencies\\logs\\'
+    for filename in os.listdir(p):
+        value = os.path.join(p, filename)
         if date1 in value:
-            path = value
+            return value
 
-#打开log文件，获取每行日志信息至lines，并关闭log文件
-with open(path, 'r', encoding='utf_8_sig') as fp:
-    lines = fp.readlines()
-fp.close()
 
-#读取1小时内log文件中的ERROR和WARNING信息
-error_text = "480\n[**ERROR**]\n"
-warning_text = "480\n[***WARNING***]\n"
-for line in lines:
-    if time1 in line:
-        if "=WARN" in line:
-            text = line[line.rfind("{level}"):]
-            warning_text = warning_text + text
-        if "=ERROR" in line:
-            text = line[line.rfind("{level}"):]
-            error_text = error_text + text
+# 获取日期时间戳
+def get_time_stamp(log_time):
+    time_array = time.strptime(log_time, '%Y-%m-%d %H:%M:%S')
+    time_stamp = float(time.mktime(time_array))
+    return time_stamp
 
-#如果日志出现报警就发送钉钉提醒
-if len(error_text) > 30:
-    print(error_text)
-    send_msg(error_text, abc)
-if len(warning_text) > 30:
-    print(warning_text)
-    send_msg(warning_text, abc)
-fp.close()
+
+# 读取log文件，按照日期分列
+def get_log_info():
+    with open(get_log_path(), 'r', encoding='utf_8_sig') as fp:
+        txt = fp.read()
+    line = txt.split('[')
+    return line[1:]
+
+
+# 读取60分钟内log文件中的ERROR和WARNING信息
+def get_main_info():
+    warning, error = set(), set()  # 定义集合筛选重复报错信息
+    for i in get_log_info():
+        delta_minute = (time.time() - get_time_stamp(i[0:19])) / 60
+        if delta_minute <= 60:  # 60分钟内的日志筛选
+            text = i[i.rfind('{msg}='):]
+            if '=WARN' in i:
+                if 'Opening stream from IStorage' in i:  # 筛选了无关报错信息，未找到报错原因
+                    pass
+                else:
+                    warning.add(text)
+            if '=ERROR' in i:
+                error.add(text)
+    return warning, error
+
+
+# 如果日志出现报警就发送钉钉提醒
+def main():
+    warning_text = 'Orbitrap Exploris 480\n[***WARNING***]\n'
+    error_text = 'Orbitrap Exploris 480\n[**ERROR**]\n'
+    for a in get_main_info()[0]:
+        warning_text += a
+    for b in get_main_info()[1]:
+        error_text += b
+    if len(warning_text) > 45:
+        print(warning_text)
+        send_msg(warning_text, person())
+    if len(error_text) > 45:
+        print(error_text)
+        send_msg(error_text, person())
+
+
+if __name__ == '__main__':
+    main()
