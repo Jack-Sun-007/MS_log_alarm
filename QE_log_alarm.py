349a9a5d612dda071db7e8564de46737a925a8cf
Jack-Sun-007 <62201888+Jack-Sun-007@users.noreply.github.com>
2021-05-26 14:42:25 +0800

    Update QE_log_alarm.py
    重构代码，新增以分钟进行日志筛选

--- QE_log_alarm.py
+++ QE_log_alarm.py
@@ -1,33 +1,12 @@
 # -*- coding: utf-8 -*-
-import codecs
-import re
-import datetime
-import requests
+import os
 import json
 import time
-import os
+import requests
 
-#获取前1小时的日期和时间
-time1= (datetime.datetime.now()+datetime.timedelta(seconds=-3600)).strftime("%Y-%m-%d %H:")
 
-#将log文件按时间进行排序
-def get_log_list(file_path):
-    dir_list = os.listdir(file_path)
-    log_list = []
-    if not dir_list:
-        return
-    else:
-        # os.path.getmtime() 函数是获取文件最后修改时间
-        # os.path.getctime() 函数是获取文件最后创建时间
-        dir_list = sorted(dir_list,  key=lambda x: os.path.getmtime(os.path.join(file_path, x)),reverse = True)
-        for file in dir_list:
-            if "Thermo Exactive--" in file:
-                log_list.append(file)
-        log_path = file_path + log_list[0]
-        return log_path
-
-#定义发送钉钉报警的函数
-def send_msg(text,mobile):
+# 定义发送钉钉报警的函数
+def send_msg(text, mobile):
     url = 'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     headers = {'Content-Type': 'application/json;charset=utf-8'}
     json_text = {
@@ -44,31 +23,70 @@ def send_msg(text,mobile):
             }
     requests.post(url, json.dumps(json_text), headers=headers).content
 
-#钉钉联系人
-abc = "\"130xxxxxxxx\""
 
-#打开log文件，获取每行日志信息至lines，并关闭log文件
-p = get_log_list("C:\\Xcalibur\\system\\Exactive\\log\\")
-with open(p, 'r', encoding='utf_8_sig') as fp:
-    lines = fp.readlines()
-fp.close()
+# 钉钉联系人
+def person():
+    who = "\"132xxxxxxxx\""
+    return who
 
-#读取1小时内log文件中的ERROR和WARNING信息
-error_text = "QE\n[**ERROR**]\n"
-warning_text = "QE\n[***WARNING***]\n"
-for line in lines:
-    if time1 in line:
-        if "error" in line:
-            text = re.sub(u"\\[.*?]", "", line)
-            error_text = error_text + text
-        if "warning" in line:
-            text = re.sub(u"\\[.*?]", "", line)
-            warning_text = warning_text + text
 
-#如果日志出现报警就发送钉钉提醒
-if len(error_text) > 25:
-    print(error_text)
-    send_msg(error_text, abc)
-if len(warning_text) > 25:
-    print(warning_text)
-    send_msg(warning_text, abc)
+# 将log文件按最后修改时间进行排序，并选择时间最新的文件
+def get_log_list(file_path):
+    dir_list = os.listdir(file_path)
+    log_list = []
+    if not dir_list:
+        return
+    else:
+        dir_list = sorted(dir_list,  key=lambda x: os.path.getmtime(os.path.join(file_path, x)), reverse=True)
+        for file in dir_list:
+            if 'Thermo Exactive--' in file:
+                log_list.append(file)
+        log_path = file_path + log_list[0]
+        return log_path
+
+
+# 获取日期时间戳
+def get_time_stamp(log_time):
+    time_array = time.strptime(log_time, "%Y-%m-%d %H:%M:%S")
+    time_stamp = float(time.mktime(time_array))
+    return time_stamp
+
+
+# 读取log文件，按照日期分列
+def get_log_info():
+    p = get_log_list('C:\\Xcalibur\\system\\Exactive\\log\\')
+    with open(p, 'r', encoding='utf_8_sig') as fp:
+        txt = fp.read()
+    line = txt.split('[Time=')
+    return line[1:]
+
+
+# 读取60分钟内log文件中的ERROR和WARNING信息
+def get_main_info():
+    error_text = 'Q-Exactive \n[**ERROR**]\n'
+    warning_text = 'Q-Exactive \n[***WARNING***]\n'
+    for i in get_log_info():
+        delta_minute = (time.time() - get_time_stamp(i[0:19])) / 60
+        if delta_minute <= 60:  # 60分钟内的日志筛选
+            text = i[i.rfind('[Type='):]
+            if 'error' in i:
+                error_text += text
+            if 'warning' in i:
+                warning_text += text
+    return warning_text, error_text
+
+
+# 如果日志出现报警就发送钉钉提醒
+def main():
+    warning_text = get_main_info()[0]
+    error_text = get_main_info()[1]
+    if len(warning_text) > 40:
+        print(warning_text)
+        send_msg(warning_text, person())
+    if len(error_text) > 40:
+        print(error_text)
+        send_msg(error_text, person())
+
+
+if __name__ == '__main__':
+    main()
